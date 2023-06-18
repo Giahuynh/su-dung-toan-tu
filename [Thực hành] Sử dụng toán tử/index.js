a2ffb8ab74afd2ef70e0569cb7a23a0c55997c2b
unknown <dbp7554@gmail.com>
2023-06-18 14:48:49 +0700

    thuc hanh su dung toan tu

---
+++ [Thực hành] Sử dụng toán tử/index.js
@@ -0,0 +1,7 @@
+let inputWidth,inputHeight;
+inputWidth = prompt("Nhập chiều rộng");
+inputHeight = prompt("Nhập chiều dài");
+inputWidth = parseInt(inputWidth);
+inputHeight = parseInt (inputHeight);
+let area = inputWidth * inputHeight;
+document.write("The area is:"+ area)
